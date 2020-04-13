9e729478a6c902a87287e569ffb80e918c6a7995
Shiao Qu <shiaoqug@gmail.com>
2020-04-14 00:45:30 +0800

    Update 445. 两数相加 II.cpp

--- 445. 两数相加 II.cpp
+++ 445. 两数相加 II.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     static ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        stack<int> s1, s2;
+        stack<int> s1, s2; // 使用栈反转链表
 
         while (l1) { s1.push(l1->val); l1 = l1->next; }
         while (l2) { s2.push(l2->val); l2 = l2->next; }
@@ -18,7 +18,7 @@ public:
             if (!e1) { v1 = s1.top(); s1.pop(); } else { v1 = 0; }
             if (!e2) { v2 = s2.top(); s2.pop(); } else { v2 = 0; }
 
-            s = v1 + v2 + carry;
+            s = v1 + v2 + carry; // 求和并处理进位
             carry = s / 10;
             s %= 10;
 
